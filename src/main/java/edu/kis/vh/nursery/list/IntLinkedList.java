8c28f5e3dc5140b538b118cd3ebdfdc662f689a3
Milosz <245857@edu.p.lodz.pl>
2024-05-20 21:27:34 +0200

    6.2 Zastapienie literalow

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    public static final int RETURN_NEGATIVE = -1;
+    public static final int EMPTY_LIST = -1;
     private Node last;
     private int i;
 
@@ -26,13 +26,13 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return RETURN_NEGATIVE;
+            return EMPTY_LIST;
         return last.getValue();
     }
 
     public int pop() {
         if (isEmpty())
-            return RETURN_NEGATIVE;
+            return EMPTY_LIST;
         int ret = last.getValue();
         last = last.getPrev();
         return ret;
