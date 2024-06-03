ac62029197c4705a0ed7501056351cb6521f377b
Piotr Ledwoch <245859@edu.p.lodz.pl>
2024-06-03 02:57:56 +0200

    14.2 Got the method to the last good form

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -10,9 +10,9 @@ public class IntLinkedList {
         if (last == null)
             last = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            ast.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
         }
     }
 
@@ -27,14 +27,14 @@ public class IntLinkedList {
     public int top() {
         if (isEmpty())
             return EMPTY_LIST;
-        return last.value;
+        return last.getValue();
     }
 
     public int pop() {
         if (isEmpty())
-            return -1;
-        int ret = last.value;
-        last = last.prev;
+            return EMPTY_LIST;
+        int ret = last.getValue();
+        last = last.getPrev();
         return ret;
     }
 
@@ -42,11 +42,32 @@ public class IntLinkedList {
 
 class Node {
 
-    public int value;
-    public Node prev, next;
+    private int value;
+    private Node prev;
+    private Node next;
 
     public Node(int i) {
         value = i;
     }
 
+    public int getValue() {
+        return value;
+    }
+
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
